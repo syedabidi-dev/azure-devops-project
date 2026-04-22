99f3211f0f4359449afb66f842b8644e80c944e1
Syed Abidi <sabidi11@myseneca.ca>
2026-04-22 18:47:05 -0400

    added github actions workflow

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('Azure DevOps Project Running ??');
 });
 
-const PORT = 3000;
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
